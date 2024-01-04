4c05daba5d73e09a16cfe9e0b25b6add381757ec
Cassidy <cassidy.rouelle@gmail.com>
2024-01-04 16:40:23 +0100

    ...

--- asset/script.js
+++ asset/script.js
@@ -1,30 +1,32 @@
 let currentMoleHole;
+let score = 0;
+let timeUp = false;
 
 window.onload = () => {
     setGame();
 }
 
 function setGame (){
-    for (let i = 0; i < 9; i++){ // i va de 0 → 9, stop à 9
-
+    for (let i = 0; i < 9; i++){  // i va de 0 → 9, stop à 9
         // div id="0-8"
         let hole = document.createElement('div');
         hole.classList.add('hole')
         hole.id = i.toString();
+
+        hole.addEventListener('click', selectHole)
+
         document.getElementById('game').appendChild(hole);
     }
 
-    setInterval(setMole, 2000);
+    setInterval(setMole, 1500);
 }
 
 function getRandomHole (){
-    // 
     let num = Math.floor(Math.random() * 9);
     return num.toString();
 }
 
 function setMole (){
-
     if (currentMoleHole) {
         currentMoleHole.innerHTML = ''
     }
@@ -36,4 +38,11 @@ function setMole (){
     let num = getRandomHole()
     currentMoleHole = document.getElementById(num);
     currentMoleHole.appendChild(mole);
+}
+
+function selectHole (){
+    if (this === currentMoleHole){
+        score += 10;
+        document.getElementById('score').innerText = score.toString();
+    }
 }
